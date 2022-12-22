ba3f23a53c36daed50bb21450c50d12d11d76b3b
Dave Sutter <sutter@intransix.com>
2022-12-22 12:58:13 -0800

    update for changed extension name

--- demo/demo.ts
+++ demo/demo.ts
@@ -2,7 +2,7 @@ import {EditorView, basicSetup} from "codemirror"
 import {javascript} from "@codemirror/lang-javascript"
 import {markdown} from "@codemirror/lang-markdown"
 
-import {doubler, checkboxPlugin, regexpLinter, images, codeEvaluator} from "@sutter-dave/cmwidgetdev"
+import {doubler, checkboxPlugin, regexpLinter, images, reactiveCode} from "@sutter-dave/cmwidgetdev"
 
 
 console.log("Double 5: " + doubler(5))
@@ -12,7 +12,7 @@ console.log("Double 5: " + doubler(5))
   extensions: [
     basicSetup,
     checkboxPlugin,
-    codeEvaluator(),
+    reactiveCode(),
     markdown({defaultCodeLanguage: javascript()})
   ],
   parent: document.querySelector("#editorMD")!
